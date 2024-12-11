e04886f9a998edfe4e971a6df3f1e5821115604d
6rainstorm <6rainstormm@gmail.com>
2024-12-11 18:50:43 +0000

    Deployment improvisation for final showcase

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import streamlit as st
 # Local Modules
 import settings
 import helper
+from helper import notify_solution
 
 
 # settings
@@ -32,7 +33,7 @@ st.markdown("<br><br>", unsafe_allow_html=True)  # Adds extra space between sele
 
 # Sidebar About
 st.sidebar.markdown("# About Us")
-st.sidebar.image("images/brainstorm.png", use_column_width=True, width=400)  # Resize the image to be larger
+st.sidebar.image("images/brainstorm.png", use_container_width=True, width=400)  # Resize the image to be larger
 st.sidebar.divider()
 
 # Data untuk anggota tim
@@ -93,7 +94,7 @@ for member in team_members:
     st.sidebar.markdown(f"### {member['name']}")
     st.sidebar.markdown(f"**{member['role']}**")
     st.sidebar.markdown(f"[LinkedIn]({member['linkedin']})" "  " f"[Instagram]({member['instagram']})")
-    st.sidebar.image(member['photo'], use_column_width=True)
+    st.sidebar.image(member['photo'], use_container_width=True)
     st.sidebar.divider()
 
 source_img = None
@@ -103,12 +104,12 @@ if selected_option == settings.HOME:
     helper.helpFunction()
     
 elif selected_option == settings.IMAGE:
-    tab1, tab2 = st.tabs(["Upload", "Buka Kamera"])
+    tab1, tab2 = st.tabs(["Upload", "Open Camera"])
     with tab1:
         # Slider untuk mengatur confidence
-        confidence = st.slider("Atur Confidence Level:", min_value=0.0, max_value=1.0, value=0.4, step=0.05)
+        confidence = st.slider("Set Confidence Level:", min_value=0.0, max_value=1.0, value=0.4, step=0.05)
         source_img = st.file_uploader(
-            "Silahkan Mengupload Gambar", type=("jpg", "jpeg", "png"))
+            "Please Upload an Image", type=("jpg", "jpeg", "png"))
         
         col1, col2 = st.columns(2)
         res_plotted = None
@@ -117,57 +118,109 @@ elif selected_option == settings.IMAGE:
                 if source_img is None:
                     default_image_path = str(settings.DEFAULT_IMAGE)
                     default_image = PIL.Image.open(default_image_path)
-                    st.image(default_image_path, caption="Gambar Awal",
-                            use_column_width=True)
+                    st.image(default_image_path, caption="Original Image",
+                            use_container_width=True)
                 else:
                     uploaded_image = PIL.Image.open(source_img)
-                    st.image(source_img, caption="Gambar Awal",
-                            use_column_width=True)
+                    st.image(source_img, caption="Original Image",
+                            use_container_width=True)
                     
                     # Tombol Detect Objects di sini
-                    if st.button('Deteksi'):
+                    if st.button('Detection'):
                         res = model.predict(uploaded_image, conf=confidence)
                         boxes = res[0].boxes
                         res_plotted = res[0].plot()[:, :, ::-1]
 
             except Exception as ex:
-                st.error("Ada Kesalahan Saat Membaca File")
+                st.error("An Error Occurred While Reading the File")
                 st.error(ex)
 
         with col2:
             if source_img is None:
                 default_detected_image_path = str(settings.DEFAULT_DETECT_IMAGE)
-                default_detected_image = PIL.Image.open(
-                    default_detected_image_path)
-                st.image(default_detected_image_path, caption='Gambar Hasil Deteksi',
-                        use_column_width=True)
+                default_detected_image = PIL.Image.open(default_detected_image_path)
+                st.image(default_detected_image_path, caption='Detection Result Image', use_container_width=True)
             else:
                 if res_plotted is not None:
-                    st.image(res_plotted, caption='Gambar Hasil Deteksi')
-                    
-                    class_indices = set(boxes.cls.tolist())
-                    unique_labels = [settings.CLASS_NAME[idx] for idx in class_indices]
+                    st.image(res_plotted, caption='Detection Result Image')
 
-                    with st.expander("Hasil Deteksi"):
-                        if unique_labels:
-                            st.success(', '.join(unique_labels))
-                        else:
-                            st.warning("Tidak Ada Objek Yang Terdeteksi")
-                else:
-                    st.empty()
+                    # Get detected labels
+                    class_indices = set(boxes.cls.tolist())  # Get class indices from detection results
+                    unique_labels = [settings.CLASS_NAME[idx] for idx in class_indices]  # Convert indices to labels
+
+                    # Display detected labels in an expander
+                    with st.expander("Detected Labels"):
+                            if unique_labels:
+                                label_colors = {
+                                    "Snow": "background-color:rgba(65, 201, 226, 0.85);color:white;",  
+                                    "Dust": "background-color:rgba(166, 110, 56);color:white;",  # Oranye
+                                    "Bird Drop": "background-color:rgba(165, 157, 132);color:white;",  # Merah Tua
+                                    "Physical Damage": "background-color:rgba(16, 44, 87);color:white;",  # Merah
+                                    "Defective": "background-color:rgba(159,0,0,255);color:white;",  # Merah Gelap
+                                    "Non Defective": "background-color:rgba(50, 205, 50, 0.5);color:white;",  # Hijau
+                                }
+
+                                # Generate HTML labels
+                                labels_html = ""
+                                for label in unique_labels:
+                                    color_style = label_colors.get(label, "background-color:#D3D3D3;color:black;")  # Default abu-abu
+                                    labels_html += f"""
+                                    <span style="{color_style};padding:10px 10px; border-radius:5px; margin-right:5px; margin-bottom:10px; display:inline-block;">
+                                        {label}
+                                    </span>
+                                    """
+
+                                # Render labels as HTML
+                                st.markdown(
+                                    f"""
+                                    <div style="margin-top:5px;">
+                                        {labels_html}
+                                    </div>
+                                    """,
+                                    unsafe_allow_html=True
+                                )
+                            else:
+                                st.warning("No objects detected.")
+
+                    
+                    # Display solution notifications below detected labels with padding
+                    st.markdown("### Solutions:")
+                    for label in unique_labels:
+                        if label in settings.CLASS_NAME.values():
+                            message, color = {
+                                "Snow": ("Panel tertutup salju, mohon dapat dibersihkan secepatnya!", "rgba(65, 201, 226, 0.85)"),  
+                                "Dust": ("Panel tertutup debu dan kotoran, mohon dapat dibersihkan secepatnya!", "rgba(166, 110, 56, 0.9)"),  
+                                "Bird Drop": ("Panel terdapat tai manuk, mohon dapat dibersihkan secepatnya!", "rgba(165, 157, 132)"),  
+                                "Physical Damage": ("Panel rusak! Wajib Diganti jika mengalami defective. Jika tidak, panel rusak tetapi masih dapat digunakan.", "rgba(16, 44, 87)"),  
+                                "Defective": ("Panel rusak parah! Segera ganti untuk menghindari kerusakan lebih lanjut.", "rgba(159,0,0,255)"), 
+                                "Non Defective": ("Bagus! Tetap jaga kondisi solar panel.", "rgba(50, 205, 50, 0.5)"),  
+                            }[label]
+                            st.markdown(
+                                f"""
+                                <div style="background-color:{color};padding:15px;margin-bottom:10px;border-radius:5px;">
+                                    <b>{message}</b>
+                                </div>
+                                """,
+                                unsafe_allow_html=True
+                            )
+
+
+
+
+
     with tab2:
         # Slider untuk mengatur confidence
-        confidence = st.slider("Atur Confidence Level (Kamera):", min_value=0.0, max_value=1.0, value=0.4, step=0.05)
+        confidence = st.slider("Adjust Confidence Level (Camera):", min_value=0.0, max_value=1.0, value=0.4, step=0.05)
         helper.take_picture(confidence, model)
 
 # Jika pilihan video
 elif selected_option == settings.VIDEO:
-    tab1, tab2 = st.tabs(["Upload", "Sumber Asal"])
+    tab1, tab2 = st.tabs(["Upload", "Original Source"])
     with tab1:
         # Slider untuk mengatur confidence
         confidence = st.slider(
-            "Atur Confidence Level (Video):", 
-            min_value=0.0, 
+            "Set Confidence Level (Video):", 
+            min_value=0.0,
             max_value=1.0, 
             value=st.session_state.get("video_confidence", 0.4), 
             step=0.05,
@@ -179,7 +232,7 @@ elif selected_option == settings.VIDEO:
     with tab2:
         # Slider untuk mengatur confidence
         confidence = st.slider(
-            "Atur Confidence Level (Sumber Asal):", 
+            "Set Confidence Level (Original Source):", 
             min_value=0.0, 
             max_value=1.0, 
             value=st.session_state.get("source_confidence", 0.4), 
@@ -193,7 +246,7 @@ elif selected_option == settings.VIDEO:
 elif selected_option == settings.WEBCAM:
     # Slider untuk mengatur confidence
     confidence = st.slider(
-        "Atur Confidence Level (Real-time):", 
+        "Set Confidence Level (Real-time):", 
         min_value=0.0, 
         max_value=1.0, 
         value=st.session_state.get("webcam_confidence", 0.4), 
--- helper.py
+++ helper.py
@@ -20,6 +20,31 @@ def load_model(model_path):
     return model
 
 
+import streamlit as st
+
+def notify_solution(labels):
+    notification_mapping = {
+        "Snow": ("Panel tertutup salju, mohon dapat dibersihkan secepatnya!", "#098eb4"),
+        "Dust": ("Panel tertutup debu dan kotoran, mohon dapat dibersihkan secepatnya!", "#098eb4"),
+        "Bird Drop": ("Panel terdapat tai manuk, mohon dapat dibersihkan secepatnya!", "#098eb4"),
+        "Physical Damage": ("Panel rusak! Wajib Diganti jika mengalami defective. Jika tidak, panel rusak tetapi masih dapat digunakan.", "#098eb4"),
+        "Defective": ("Panel rusak parah! Segera ganti untuk menghindari kerusakan lebih lanjut.", "#ebbab9"),
+        "Non Defective": ("Bagus! Tetap jaga kondisi solar panel.", "#098eb4"),
+    }
+    for label in labels:
+        if label in notification_mapping:
+            message, color = notification_mapping[label]
+            # Gunakan HTML untuk mengatur background
+            st.markdown(
+                f"""
+                <div style="background-color:{color};padding:10px;border-radius:5px;">
+                    <b>{message}</b>
+                </div>
+                """,
+                unsafe_allow_html=True
+            )
+
+
 
 def showDetectFrame(conf, model, st_frame, image, is_display_tracking=None, tracker=None):
     # Mengubah ukuran frame agar lebih kecil untuk deteksi yang lebih cepat
@@ -66,7 +91,7 @@ class VideoTransformer(VideoTransformerBase):
 def check_camera_source(source=0):
     vid_cap = cv2.VideoCapture(source)
     if not vid_cap.isOpened():
-        raise ValueError(f"Kamera tidak tersedia pada sumber {source}")
+        raise ValueError(f"Camera is not available at the source {source}")
     vid_cap.release()
     check_camera_source(settings.WEBCAM_PATH)
 
@@ -120,16 +145,16 @@ def process_frame_async(vid_cap, conf, model, st_frame):
         time.sleep(0.03)  # Menunggu sejenak sebelum frame berikutnya diproses
         
 def play_stored_video(conf, model):
-    source_vid = st.selectbox("Silahkan Pilih Video yang Sudah Disediakan", settings.VIDEOS_DICT.keys())
+    source_vid = st.selectbox("Please Select a Video from the Provided Options", settings.VIDEOS_DICT.keys())
     video_path = settings.VIDEOS_DICT.get(source_vid)
     
     if video_path is None:
-        st.error("Video tidak ditemukan.")
+        st.error("Video not found.")
         return
     
     vid_cap = cv2.VideoCapture(str(video_path))
     if not vid_cap.isOpened():
-        st.error(f"Tidak dapat membuka video: {video_path}")
+        st.error(f"Unable to open video: {video_path}")
         return
     
     st_frame = st.empty()
@@ -146,14 +171,14 @@ def play_stored_video(conf, model):
 
             
 def take_picture(conf, model):
-    picture = st.camera_input("Silahkan Mengambil Gambar")
+    picture = st.camera_input("Please Take a Picture")
 
     if picture:
         with NamedTemporaryFile(delete=False, suffix='.mp4') as temp_file:
             temp_file.write(picture.read())
             temp_pict_path = temp_file.name
             
-        if st.button('Deteksi Foto'):
+        if st.button('Detect Photo'):
             try:
                 vid_cap = cv2.VideoCapture(temp_pict_path)
                 st_frame = st.empty()
